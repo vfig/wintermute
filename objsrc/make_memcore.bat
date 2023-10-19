f9ccd5361170a7a6f6f8ca6ca39fe80c082a973c
Andy Durdin <me@andy.durdin.net>
2023-10-19 17:19:22 +0100

    test_memcore2: lod 0 models with joints.

--- objsrc/make_memcore.bat
+++ objsrc/make_memcore.bat
@@ -1,6 +1,10 @@
 @echo off
-REM bsp mem4x4x4lod1.e mem4x4x4lod1.bin -N -l0 -V -o -M0.866
+bsp memcorea.e memcorea.bin -l3 -V -o -M0.866
+bsp memcoreb.e memcoreb.bin -l3 -V -o -M0.866
+bsp mem4x4x4lod1.e mem4x4x4lod1.bin -N -l0 -V -o -M0.866
 bsp mem4x4x4lod2.e mem4x4x4lod2.bin -l0 -V -o -M0.866
+copy /y memcorea.bin ..\obj\
+copy /y memcoreb.bin ..\obj\
 copy /y mem4x4x4lod1.bin ..\obj\
 copy /y mem4x4x4lod2.bin ..\obj\
 copy /y memcore.png ..\obj\txt16
