e2ea86378a3e32bd51a39f5e0aee23783d8596c4
Andy Durdin <me@andy.durdin.net>
2023-10-19 09:09:14 +0100

    test_memcore2: lod models for memory cores.

---
+++ objsrc/make_memcore.bat
@@ -0,0 +1,9 @@
+@echo off
+REM bsp mem4x4x4lod1.e mem4x4x4lod1.bin -N -l0 -V -o -M0.866
+bsp mem4x4x4lod2.e mem4x4x4lod2.bin -l0 -V -o -M0.866
+copy /y mem4x4x4lod1.bin ..\obj\
+copy /y mem4x4x4lod2.bin ..\obj\
+copy /y memcore.png ..\obj\txt16
+copy /y memcorelod2a.png ..\obj\txt16
+copy /y memcorelod2b.png ..\obj\txt16
+copy /y memcorelod2c.png ..\obj\txt16
